de29f2f793ad45fe02e93f7fe2081ebe03062aaf
Juan Bautista Colucci <juanbautistacolucci@gmail.com>
2022-06-21 15:34:12 -0300

    dom

--- js/main.js
+++ js/main.js
@@ -1,65 +1,84 @@
+let btnCalcular = document.getElementById("calcular");
+
+let wind = parseInt(document.getElementById("wind").value)
+let swell = parseInt(document.getElementById("swell").value)
+let period = parseInt(document.getElementById("period").value)
+
+let puntaje 
+let nombre
+
+let resultado1
+let resultado2
+let resultado3
+
 function ola() {
-    wind = parseInt(prompt("Ingrese un número segun la direccion del viento: 1. offshore 2. sideshore 3. onshore"));
-    swell = parseFloat(prompt("Ingresa el ángulo que forma el swell con la costa, 0 a 90, siendo 90 lo mas perpendicular a la costa posible"));
-    period = parseFloat(prompt("Ingrese el periodo de olas en segundos"));
     if (wind == 1) {
-        wind = 5
+        resultado1 = 5
     } else if (wind == 2) {
-        wind = 2.5
+        resultado1 = 2.5
     } else if (wind == 3) {
-        wind = 1
+        resultado1 = 1
     };
     if (swell <= 30) {
-        swell = 1
+        resultado2 = 1
     } else if (swell <= 50) {
-        swell = 2.5
+        resultado2 = 2.5
     } else if (swell <= 70) {
-        swell = 4
+        resultado2 = 4
     } else if (swell > 70) {
-        swell = 5
+        resultado2 = 5
     };
     if (period <= 5) {
-        period = 1
+        resultado3 = 1
     } else if (period < 7) {
-        period = 2.5
+        resultado3 = 2.5
     } else if (period < 9) {
-        period = 3.5
+        resultado3 = 3.5
     } else if (period >= 9) {
-        period = 5
+        resultado3 = 5
     };
-};
-
+}
 class Puntajes {
     constructor(nombre, puntaje) {
         this.nombre = nombre;
         this.puntaje = puntaje;
     }
 }
-
 const historial = [];
 
-let pregunta = prompt("Hola, si queres conocer la calidad de la ola que estas por surfear presiona enter, para salir ingresa NO.").toUpperCase();
-
-while (pregunta != "NO") {
-    ola();
-    let puntaje = (wind + swell + period) / 3;
-    let nombre = prompt("ingresa el nombre con el que quieres guardar el puntaje en el historial");
+btnCalcular.addEventListener("click", () => {
+    wind = document.getElementById("wind").value
+    swell = document.getElementById("swell").value
+    period = document.getElementById("period").value
+    nombre = document.getElementById("nombre").value;
+    ola()
+    puntaje = (resultado1 + resultado2 + resultado3) / 3;
     const puntajeGuardado = new Puntajes(nombre, puntaje);
     historial.push(puntajeGuardado);
 
-    if (isNaN(puntaje)) {
-        alert("Datos mal ingresado, prueba con un numero");
-    } else {
-        alert("el puntaje de la ola es de " + puntaje);
-    }
-
+    let guardados = document.getElementById("guardados")
+    guardados.innerHTML = ""
     for (const guardado of historial) {
-        alert("Se guardo en el historial tu puntaje de " + guardado.puntaje + " con el nombre de " + guardado.nombre);
+        let li = document.createElement("li");
+        li.innerText = `${guardado.nombre} ${guardado.puntaje}`
+        guardados.appendChild(li)
     };
+    
+    let resultado = document.getElementById("resultado")
+    resultado.innerText = "Resultado: " + puntaje;
+});
 
-    const buscar = historial.find((el) => el.nombre === prompt("Para buscar un puntaje en el historial, ingresa el nombre con el que lo guardaste"));
+let btnBuscar = document.getElementById("buscar");
+let busqueda
 
-    alert("buscate el puntaje " + buscar.nombre + " de " + buscar.puntaje)
+btnBuscar.addEventListener("click", () => {
+    busqueda = document.getElementById("busqueda").value;
+    const filtradora = historial.filter((el) => el.nombre === busqueda)
+    filtrados.innerHTML = ""
+    for (const filtrado of filtradora) {
+        let li = document.createElement("li");
+        li.innerText = `${filtrado.nombre} ${filtrado.puntaje}`
+        filtrados.appendChild(li)
+    };
+});
 
-    pregunta = prompt("Hola, si queres conocer la calidad de la ola que estas por surfear presiona enter, para salir ingresa NO.");
-};

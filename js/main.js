2eb638a70b5a635dd2350ad54b387d1e4bbb49fe
Juan Bautista Colucci <juanbautistacolucci@gmail.com>
2022-08-24 17:12:05 -0300

    array

--- js/main.js
+++ js/main.js
@@ -1,91 +1,60 @@
-// Variables
-let btnCalcular = document.getElementById("calcular");
-let btnBuscar = document.getElementById("buscar");
-
-let wind
-let swell
-let period
-
-let puntaje
-let nombre
+alert("Calculador de calidad de olas, califica las olas del 1 al 5");
+let wind = parseInt(prompt("Ingrese un número segun la direccion del viento: 1. offshore 2. sideshore 3. onshore"));
+let swell = parseInt(prompt("Ángulo que forma el swell con la costa, 0 a 90"));
+let period = parseInt(prompt("Periodo de olas en segundos"));
+let nombre = prompt("nombre con el que desea guardar el puntaje");
 
 let resultado1
 let resultado2
 let resultado3
 
-// Función para evaluar los parametros
-function ola() {
-    if (wind == 1) {
-        resultado1 = 5
-    } else if (wind == 2) {
-        resultado1 = 2.5
-    } else if (wind == 3) {
-        resultado1 = 1
-    };
-    if (swell <= 30) {
-        resultado2 = 1
-    } else if (swell <= 50) {
-        resultado2 = 2.5
-    } else if (swell <= 70) {
-        resultado2 = 4
-    } else if (swell > 70) {
-        resultado2 = 5
-    };
-    if (period <= 5) {
-        resultado3 = 1
-    } else if (period < 7) {
-        resultado3 = 2.5
-    } else if (period < 9) {
-        resultado3 = 3.5
-    } else if (period >= 9) {
-        resultado3 = 5
-    };
-}
+let puntaje
 
-// Contructor de objetos y array para guardarlos
-class Puntajes {
+const historial = [];
+
+if (wind == 1) {
+    resultado1 = 5
+} else if (wind == 2) {
+    resultado1 = 2.5
+} else if (wind == 3) {
+    resultado1 = 1
+};
+if (swell <= 30) {
+    resultado2 = 1
+} else if (swell <= 50) {
+    resultado2 = 2.5
+} else if (swell <= 70) {
+    resultado2 = 4
+} else if (swell > 70) {
+    resultado2 = 5
+};
+if (period <= 5) {
+    resultado3 = 1
+} else if (period < 7) {
+    resultado3 = 2.5
+} else if (period < 9) {
+    resultado3 = 3.5
+} else if (period >= 9) {
+    resultado3 = 5
+};
+class guardarPuntaje {
     constructor(nombre, puntaje) {
         this.nombre = nombre;
         this.puntaje = puntaje;
     }
-}
-const historial = [];
+};
 
-// Eventos botones "calcular" y "buscar"
-btnCalcular.addEventListener("click", () => {
-    wind = document.getElementById("wind").value
-    swell = document.getElementById("swell").value
-    period = document.getElementById("period").value
-    nombre = document.getElementById("nombre").value;
-    ola()
+function calculador() {
     puntaje = (resultado1 + resultado2 + resultado3) / 3;
-    const puntajeGuardado = new Puntajes(nombre, puntaje);
+    alert("el puntaje de la ola es " + puntaje)
+};
 
-    if (isNaN(puntaje)) {
-        alert("Dato mal ingresado")
-    } else {
-        historial.push(puntajeGuardado);
-        const almacenado = JSON.stringify(puntajeGuardado);
-        localStorage.setItem(`${puntajeGuardado.nombre}`, `${almacenado}`);
+function guardar() {
+    let guardado = new guardarPuntaje(nombre, puntaje)
+    historial.push(guardado)   
+};
 
-        let guardados = document.getElementById("guardados")
-        guardados.innerHTML = ""
-        for (const guardado of historial) {
-            let li = document.createElement("li");
-            li.innerText = `${guardado.nombre} ${guardado.puntaje}`
-            guardados.appendChild(li)
-        };
+calculador();
+guardar();
 
-        let resultado = document.getElementById("resultado")
-        resultado.innerText = `Resultado: ${puntaje}`
-    }
-});
-
-btnBuscar.addEventListener("click", () => {
-    let busqueda = document.getElementById("busqueda").value;
-    buscado = JSON.parse(localStorage.getItem(busqueda));
-
-    let li = document.createElement("li");
-    li.innerText = `${buscado.nombre} ${buscado.puntaje}`
-    buscados.appendChild(li)
-});
+historial.forEach()

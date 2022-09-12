1c8604ba44409f8a95b4b5f66fd8d36b5da32fc1
Juan Bautista Colucci <juanbautistacolucci@gmail.com>
2022-09-12 15:04:49 -0300

    historial

--- js/main.js
+++ js/main.js
@@ -1,13 +1,8 @@
+let nombre
+let fecha
 let wind
 let swell
 let period
-/* let nombre = document.getElementById("date").value */
-
-let btnCalcular = document.getElementById("calcular")
-let btnGuardar = document.getElementById("guardar")
-let btnBuscar = document.getElementById("buscar")
-
-let pResultado = document.getElementById("resultado")
 
 let resultado1
 let resultado2
@@ -15,10 +10,23 @@ let resultado3
 
 let puntaje
 
+let btnCalcular = document.getElementById("calcular")
+let btnGuardar = document.getElementById("guardar")
+let btnNombre = document.getElementById("btnNombre")
+let btnFecha = document.getElementById("btnFecha")
+
+
+let textResultado = document.getElementById("resultado")
+let textHistorial = document.getElementById("historial")
+
+let buscarNombre
+let buscarFecha
+
 const historial = [];
-class guardarPuntaje {
-    constructor(nombre, puntaje) {
+class puntajeGuardado {
+    constructor(nombre, fecha, puntaje) {
         this.nombre = nombre;
+        this.fecha = fecha;
         this.puntaje = puntaje;
     }
 };
@@ -57,25 +65,91 @@ function calcular(e){
     };
    
     puntaje = (resultado1 + resultado2 + resultado3) / 3;
-    pResultado.innerHTML=`La calidad de la ola es de ${puntaje} estrellas`
+    textResultado.innerHTML=`La calidad de la ola es de ${puntaje} estrellas`
 };
 
 btnGuardar.addEventListener("click", guardar)
-function guardar() {
-    let guardado = new guardarPuntaje(nombre, puntaje)
-    historial.push(guardado)   
+function guardar(e) {
+    e.preventDefault();
+
+    nombre = document.getElementById("name").value
+    fecha = document.getElementById("date").value;
+
+    let guardado = new puntajeGuardado(nombre, fecha, puntaje)
+    historial.push(guardado)
+    let historialJson = JSON.stringify(historial)
+    localStorage.setItem("historial", historialJson)
 };
 
-/* function calcular() {
-    puntaje = (resultado1 + resultado2 + resultado3) / 3;
-    alert("el puntaje de la ola es " + puntaje)
-}; 
-
-function guardar() {
-    let guardado = new guardarPuntaje(nombre, puntaje)
-    historial.push(guardado)   
+btnFecha.addEventListener("click", buscarPorFecha)
+function buscarPorFecha(e) {
+    e.preventDefault();
+    
+    let getHistorial = localStorage.getItem("historial")
+    let historialParse = JSON.parse(getHistorial)
+    
+    buscarFecha = document.getElementById("buscarFecha").value
+    
+    const historialFiltrado = historialParse.filter((el) => el.fecha == buscarFecha)
+    textHistorial.innerHTML=``
+    for (const item of historialFiltrado) {
+        let li = document.createElement("li")
+        li.innerHTML= `${item.fecha} ${item.puntaje}`
+        textHistorial.appendChild(li)
+    }
 };
-*/
 
-/* calculador();
-guardar(); */
+btnNombre.addEventListener("click", buscar)
+function buscar(e) {
+    e.preventDefault();
+
+    let getHistorial = localStorage.getItem("historial")
+    let historialParse = JSON.parse(getHistorial)
+
+    buscarNombre = document.getElementById("buscarNombre").value
+    
+    const historialFiltrado = historialParse.filter((el) => el.nombre == buscarNombre)
+    textHistorial.innerHTML=``
+    for (const item of historialFiltrado) {
+        let li = document.createElement("li")
+        li.innerHTML= `${item.nombre} ${item.puntaje}`
+        textHistorial.appendChild(li)
+    }
+};
+
+
+/* btnBuscar.addEventListener("click", buscar)
+function buscar(e) {
+    e.preventDefault();
+
+    let getHistorial = localStorage.getItem("historial")
+    let historialParse = JSON.parse(getHistorial)
+
+    buscarNombre = document.getElementById("buscarNombre").value
+    
+    const historialFiltrado = historialParse.filter((el) => el.nombre == buscarNombre)
+    textHistorial.innerHTML=``
+    for (const item of historialFiltrado) {
+        let li = document.createElement("li")
+        li.innerHTML= `${item.nombre} ${item.puntaje}`
+        textHistorial.appendChild(li)
+    }
+}; */
+
+/* if (buscarFecha = undefined){
+    const historialFiltrado = historialParse.filter((el) => el.nombre == buscarNombre)
+    textHistorial.innerHTML=``
+    for (const item of historialFiltrado) {
+        let li = document.createElement("li")
+        li.innerHTML= `${item.nombre} ${item.puntaje}`
+        textHistorial.appendChild(li)
+    }
+} else if (buscarNombre == undefined){
+    const historialFiltrado = historialParse.filter((el) => el.fecha == buscarFecha)
+    textHistorial.innerHTML=``
+    for (const item of historialFiltrado) {
+        let li = document.createElement("li")
+        li.innerHTML= `${item.fecha} ${item.puntaje}`
+        textHistorial.appendChild(li)
+    }
+} */
